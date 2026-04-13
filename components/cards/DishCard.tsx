108d18c1557c7b434d93b66b2d7574bb988cef76
zuranmateo <mateo.zuran@scv.si>
2026-04-13 15:59:42 +0200

    Added dish card

--- components/cards/DishCard.tsx
+++ components/cards/DishCard.tsx
@@ -1,8 +1,21 @@
 "use client"
 import { AddToCart } from '@/lib/actions'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function DishCard({dish}: {dish: any}){
+
+  const [loading, setLoading] = useState(false);
+
+  const handleAddToCart = async () => {
+    setLoading(true);
+
+    try {
+      await AddToCart(dish.slug);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div>
         <h2>{dish.name}</h2>
@@ -11,7 +24,7 @@ export default function DishCard({dish}: {dish: any}){
             {/**description, price, image, (ingrediants bom js, ti samo naredi plac), preparationTime, isPopular, isAvailable */}
         </div>
         <div>
-            <button onClick={() => AddToCart(dish.slug)}>Add to cart</button>
+            <button onClick={handleAddToCart} disabled={loading}>{loading ? "adding..." : "add to cart"}</button>
         </div>
     </div>
   )
