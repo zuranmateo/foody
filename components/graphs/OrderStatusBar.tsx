294075a6f0995644b7a94d9cf505a49c1a15f81d
zuranmateo <mateo.zuran@scv.si>
2026-04-21 08:25:48 +0200

    name change on graph

--- components/graphs/OrderStatusBar.tsx
+++ components/graphs/OrderStatusBar.tsx
@@ -20,7 +20,7 @@ export default function OrderStatusBar({
     return (
         <div style={{ width: '80%', maxWidth: "400", height: 250, aspectRatio: 1.618, }} className="shadow-2xl rounded-2xl border py-5 my-5 mx-2">
                     <h2 className="text-2xl px-4">
-                        MOST ORDERED DISHES
+                        ORDER STATUS
                     </h2>
                       <ResponsiveContainer>
                         <BarChart data={segments} margin={{ top: 20, right: 20, left: 10, bottom: 30 }}>
